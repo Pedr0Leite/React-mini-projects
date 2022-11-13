38fd8ea0e1a2b555f149c67d311fbe1f4a09fa74
Leite <pedro.leite@fujitsu.com>
2022-11-13 16:32:04 +0000

    nothing major

--- MERN-exercise-app/backend/server.js
+++ MERN-exercise-app/backend/server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
-const workoutRoutes = require("./routes/workouts");
 const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 
 const PORT = process.env.PORT;
 
